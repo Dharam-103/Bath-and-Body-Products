88f04584f6c2c226f94436f2e0e62fb1ebab78db
Dharmendra yadav <kumaediren786@gmail.com>
2022-09-28 11:40:26 -0700

    add navbar file

---
+++ components/nav.js
@@ -0,0 +1,144 @@
+ 
+ function nav(){
+
+    return `  <div id="top-nav">
+    <div id="first-div">
+        <p>POINTS. PERKS. EXCLUSIVES.</p>
+        <a href="#">Join Rewards!</a>
+    </div>
+    <div id="second-div">
+        <i class="fa-sharp fa-solid fa-location-dot"></i>
+        <p>PICK UP IN STORE</p>
+        <a href="#">Set Store</a>
+    </div>
+</div>
+
+<!-- middle part of navbar section -->
+
+<div id="middle-nav">
+    <div id="show-border">
+       <img src="https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/on/demandware.static/Sites-BathAndBodyWorks-Site/-/default/dw22126b8b/images/svg-icons/Logos-main.svg?yocs=o_s_" alt="Error">
+    </div>
+    <div>
+        <input type="text" id="search" placeholder="search ...">
+        <a href=""><i class="fa-solid fa-magnifying-glass"></i></a>
+       <a href="">
+        <i class="fa-solid fa-user"></i>
+         <div id="user-show">
+            <ul>
+              <li><a href="">Signin or signup</a></li>
+              <li><a href="">Order tracking</a></li>
+              <li><a href="">My Auto Refresh</a></li>
+              <li><a href="">My Love-It List</a></li>
+            </ul>
+         </div>
+       </a> 
+       <a href=""><i class="fa-solid fa-cart-plus"></i></a> 
+    </div>
+ </div>
+
+<!-- navbar all page heading -->
+<div id="page-heading">
+    <ul>
+     <li><a href="">TOP OFFERS</a></li>
+     <li id="item-2"> 
+        <a href="">BODY CARE</a>
+        <div id="dropdown">
+           <ul>
+              <li ><a href="">NEW & NOW</a></li>
+              <li><a href="">BATH & SHOWER</a></li>
+              <li><a href="">MOISTURIZERS</a></li>
+              <li><a href="">FRAGRANCE</a></li>
+              <li><a href="">EXPLORE BODY CARE</a></li>
+           </ul>
+        </div>
+     </li>
+     <li id="item-3"> 
+        <a href="">CANDLES</a> 
+        <div id="candles">
+           <ul>
+              <li ><a href="">ALL CANDLES</a></li>
+              <li><a href="">FEATURED</a></li>
+              <li><a href="">EXPLORE CANDLES</a></li>
+              <li><a href="">BEST OFFERS</a></li>
+           </ul>
+        </div>
+     </li>
+     <li id="item-4"> 
+        <a href="">HOME FRAGRANCE</a>
+        <div id="fragrance">
+           <ul>
+              <li ><a href="">AIR FRAGRANCE</a></li>
+              <li><a href="">FEATURED</a></li>
+              <li><a href="">EXPLORE HOME FRAGRANCE</a></li>
+              <li><a href="">BEST OFFERS</a></li>
+           </ul>
+        </div>
+     
+     </li>
+     <li id="item-5">
+        <a href="">HANDSOAPS & SANITIZERS</a>
+        <div id="Soap">
+           <ul>
+              <li ><a href="">HAND SOAPS</a></li>
+              <li><a href="">FEATURED</a></li>
+              <li><a href="">HAND SANITIGERS</a></li>
+              <li><a href="">BEST OFFERS</a></li>
+           </ul>
+        </div>
+     
+     </li>
+     <li id="item-6">
+        <a href="">MEN'S SHOP</a>
+        <div id="men-shop">
+           <ul>
+              <li ><a href="">MEN'S SHOP</a></li>
+              <li><a href="">EXPLORE MEN'S SHOP</a></li>
+           </ul>
+        </div>
+     </li>
+     <li id="item-7">
+        <a href="">GIFTS</a>
+        <div id="gift">
+           <ul>
+              <li ><a href="">NEW & NOW</a></li>
+              <li><a href="">BATH & SHOWER</a></li>
+              <li><a href="">MOISTURIZERS</a></li>
+              <li><a href="">FRAGRANCE</a></li>
+              <li><a href="">EXPLORE BODY CARE</a></li>
+           </ul>
+        </div>
+     </li>
+     <li id="item-8">
+        <a href="" class="active">FALL SHOP</a>
+        <div id="fall-shop">
+           <ul>
+              <li ><a href="">NEW & NOW </a></li>
+              <li><a href="">EXPLORE FALL SHOP</a></li>
+           </ul>
+        </div>
+     </li>
+    </ul>
+    
+</div>
+
+<!-- navbar last  -->
+<div id="nav-last">
+    <P>My Bath & Body Works Rewards is now available nationwide!</P>
+    <p id="show-popup"> JOIN THE VIPS</p>
+</div>
+ 
+<!-- popup -->
+<div class="pop">
+    <div>
+       <h3>JOIN THE VIPS</h3>
+       <i class="fa-sharp fa-solid fa-xmark"></i>
+    </div>
+    <div>
+      <p>It's finally here: My Bath & Body Works Rewards <a href="">JOIN THE VIPS</a></p>
+    </div>
+</div>`
+
+ }
+
+ export { nav };
---
+++ navbar.js
@@ -0,0 +1,284 @@
+
+ import { nav } from './components/nav.js' 
+
+ const nav_div=document.getElementById('navbar') 
+
+ nav_div.innerHTML=nav() 
+
+
+//  Showing img of bath of border
+
+const show =document.getElementById('show-border') 
+
+show.addEventListener('mouseover',function(){
+    showborder()
+})
+
+function showborder(){
+
+    show.classList.add('appear')
+}
+
+const remove =document.querySelector('#show-border') 
+
+remove.addEventListener('mouseout',function(){
+    removeborder()
+})
+
+function removeborder(){
+
+    show.classList.remove('appear');
+}
+
+//  all page of border js
+
+const pageHeading =document.getElementById('page-heading') 
+
+pageHeading.addEventListener('click',function(){
+    pageborder()
+})
+
+function pageborder(){
+
+    pageHeading.classList.add('appear')
+}
+
+const pageOut =document.getElementById('page-heading') 
+
+pageOut.addEventListener('mouseout',function(){
+    pageOutBorder()
+})
+
+function pageOutBorder(){
+
+    pageOut.classList.remove('appear')
+}
+
+// showing popup
+
+const popup= document.querySelector('#show-popup')
+
+const pop= document.querySelector('.pop')
+
+popup.addEventListener('mouseover',()=>{
+
+    display()
+})
+
+const display=()=>{
+    pop.style.display='block'
+}
+
+const removePop= document.querySelector('#show-popup')
+
+const hide= document.querySelector('.pop')
+
+removePop.addEventListener('mouseout',()=>{
+
+    update()
+})
+
+const update=()=>{
+    hide.style.display='none'
+}
+
+// Body care dropdown
+
+const show_Div=document.getElementById('dropdown') 
+
+const selected_item=document.getElementById('item-2') 
+
+selected_item.addEventListener('mouseover',()=>{
+    divAppear()
+})
+
+const divAppear=()=>{
+    selected_item.style.textDecoration='underline'
+   show_Div.style.display='block'
+}
+
+selected_item.addEventListener('mouseout',()=>{
+    disappear()
+})
+
+const disappear=()=>{
+
+   show_Div.style.display='none'
+   selected_item.style.textDecoration='none'
+}
+
+//    candles dropdown js 
+
+const candles=document.getElementById('candles') 
+
+const item_3=document.getElementById('item-3') 
+
+item_3.addEventListener('mouseover',()=>{
+    visible()
+})
+
+const visible=()=>{
+    item_3.style.textDecoration='underline'
+   candles.style.display='block'
+}
+
+ item_3.addEventListener('mouseout',()=>{
+    unvisible()
+})
+
+const unvisible=()=>{
+   candles.style.display='none'
+   item_3.style.textDecoration='none'
+}
+
+// Fragrance related js 
+
+const fragrance=document.getElementById('fragrance') 
+
+const item_4=document.getElementById('item-4') 
+
+item_4.addEventListener('mouseover',()=>{
+    visibled()
+})
+
+const visibled=()=>{
+    item_4.style.textDecoration='underline'
+   fragrance.style.display='block'
+}
+
+ item_4.addEventListener('mouseout',()=>{
+    unvisibled()
+})
+
+const unvisibled=()=>{
+   fragrance.style.display='none'
+   item_4.style.textDecoration='none'
+}
+
+// Hand soap and sanitizer js
+
+const soap=document.getElementById('Soap') 
+
+const item_5=document.getElementById('item-5') 
+
+item_5.addEventListener('mouseover',()=>{
+     soapDiv()
+})
+
+const soapDiv=()=>{
+    item_5.style.textDecoration='underline'
+   soap.style.display='block'
+}
+
+ item_5.addEventListener('mouseout',()=>{
+    soapUndiv()
+})
+
+const  soapUndiv=()=>{
+   soap.style.display='none'
+   item_5.style.textDecoration='none'
+}
+
+//   Men's Shop js
+
+const men_shop=document.getElementById('men-shop') 
+
+const item_6=document.getElementById('item-6') 
+
+item_6.addEventListener('mouseover',()=>{
+     menShop()
+})
+
+const menShop=()=>{
+    item_6.style.textDecoration='underline'
+   men_shop.style.display='block'
+}
+
+ item_6.addEventListener('mouseout',()=>{
+    removeMen()
+})
+
+const  removeMen=()=>{
+   men_shop.style.display='none'
+   item_6.style.textDecoration='none'
+}
+
+// Gift releted js
+
+const gift=document.getElementById('gift') 
+
+const item_7=document.getElementById('item-7') 
+
+item_7.addEventListener('mouseover',()=>{
+     addGift()
+})
+
+const addGift=()=>{
+    item_7.style.textDecoration='underline'
+   gift.style.display='block'
+}
+
+ item_7.addEventListener('mouseout',()=>{
+    removeGift()
+})
+
+const  removeGift=()=>{
+   gift.style.display='none'
+   item_7.style.textDecoration='none'
+}
+
+//  fall shop related js 
+
+const fall=document.getElementById('fall-shop') 
+
+const item_8=document.getElementById('item-8') 
+
+item_8.addEventListener('mouseover',()=>{
+     addFall()
+})
+
+const addFall=()=>{
+    item_8.style.textDecoration='underline'
+   fall.style.display='block'
+}
+
+ item_8.addEventListener('mouseout',()=>{
+    removeFall()
+})
+
+const  removeFall=()=>{
+   fall.style.display='none'
+   item_8.style.textDecoration='none'
+  
+}
+
+// user icon show div
+
+const user_div= document.getElementById('user-show')
+
+const user_profile=document.querySelector('.fa-user')
+
+user_profile.addEventListener('mouseover',()=>{
+     userAppear()
+})
+
+const userAppear=()=>{
+    user_div.style.display='block'
+}
+
+user_profile.addEventListener('mouseout',()=>{
+    setTimeout( userDisappear,3000)
+})
+
+const userDisappear=()=>{
+   user_div.style.display='none'
+}
+
+
+
+
+
+
+
+
+
