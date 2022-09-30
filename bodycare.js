a42ac615c1d8215c2ff194060d4f87e28f1a62bc
Kaushik <kaubis368@gmail.com>
2022-10-01 01:51:53 +0530

    day-4

--- bodycare.js
+++ bodycare.js
@@ -1,55 +1,231 @@
-const movies=[
+const items=[
     {
-        name:"",
-        price: ,
-        rating: ,
-        img: ""
+        images:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw6d5a51f8/crop/026491606_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        titles:"Fall in Bloom",
+        sidetitles:"Fine Fragrance Mist",
+        price:17.50,
+        rating:4.5,
+    },
+    {
+        images:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw4c0be74b/crop/026520992_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        titles:"Vanilla Patchouli",
+        sidetitles:"Moisturizing Body Lotion",
+        price:16.50,
+        rating:3.5,
+    },
+    {
+        images:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwdfe3b194/crop/026551686_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        titles:"Smoked Old Fashioned",
+        sidetitles:"Body Spray",
+        price:14.50,
+        rating:1,
+    },
+    {
+        images:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwb031315c/crop/026517929_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        titles:"Sweet Whiskey",
+        sidetitles:"Ultimate Hydration Body Cream",
+        price:17.50,
+        rating:3.7,
+    },
+    {
+        images:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwd24bf42a/crop/026552802_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        titles:"Bourbon",
+        sidetitles:"3-in-1 Hair, Face & Body Wash",
+        price:15.50,
+        rating:5,
+    },
+    {
+        images:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw12e173ea/hires/026544410_alt_1.jpg?sh=413&yocs=o_s_",
+        titles:"Sweet Cinnamon Pumpkin",
+        sidetitles:"Fine Fragrance Mist",
+        price:17.50,
+        rating:3.3,
+    },
+    {
+        images:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw240e57c4/crop/026520997_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        titles:"Cinnamon Sandalwood",
+        sidetitles:"Body Wash and Foam Bath",
+        price:15.50,
+        rating:4,
+    },
+    {
+        images:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw8f1efea3/crop/026551695_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        titles:"Whiskey Reserve",
+        sidetitles:"Ultimate Hydration Body Cream",
+        price:16.50,
+        rating:2,
+    },
+    {
+        images:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwf42e31a5/crop/026528604_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        titles:"Pumpkin Pecan Waffles",
+        sidetitles:"Ultimate Hydration Body Cream",
+        price:16.50,
+        rating:4,
+    },
+    {
+        images:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw07cdfa14/hires/026501135_alt_1.jpg?sh=413&yocs=o_s_",
+        titles:"White Pumpkin & Chai",
+        sidetitles:"Shower Gel",
+        price:14.50,
+        rating:5,
+    },
+    {
+        images:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw3ef2d6ce/crop/026495990_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        titles:"Coffee & Whiskey",
+        sidetitles:"Body Spray",
+        price:14.50,
+        rating:4.1,
+    },
+    {
+        images:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw279c6757/crop/026494139_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        titles:"Sweater Weather",
+        sidetitles:"Shower Gel",
+        price:13.50,
+        rating:3.9,
+    },
+    {
+        images:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwb859bf7f/crop/026490235_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        titles:"Midnight Amber Glow",
+        sidetitles:"Fine Fragrance Mist",
+        price:17.50,
+        rating:4.05,
+    },
+    {
+        images:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dwa6813a85/crop/026491608_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        titles:"Fall in Bloom",
+        sidetitles:"Ultimate Hydration Body Cream",
+        price:17.50,
+        rating:3.7,
+    },
+    {
+        images:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw45072c4a/crop/026528617_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        titles:"Strawberry Pound Cake",
+        sidetitles:"Shower Gel",
+        price:13.50,
+        rating:3.9,
+    },
+    {
+        images:"https://cdn-fsly.yottaa.net/5d669b394f1bbf7cb77826ae/www.bathandbodyworks.com/v~4b.219/dw/image/v2/BBDL_PRD/on/demandware.static/-/Sites-master-catalog/default/dw8e559fb4/crop/026494140_crop.jpg?sw=500&sh=600&sm=fit&q=75&yocs=o_s_",
+        titles:"Ivory Cashmere",
+        sidetitles:"Shower Gel",
+        price:13.50,
+        rating:4.6,
     },
     
-];
+]
 
-localStorage.setItem("movies", JSON.stringify(movies));
-let data=JSON.parse(localStorage.getItem("movies"));
-function appendMovies(data){
-    let data_div=document.getElementById("basic") || document.getElementById("movies");
-    //data_div.innerHTML=null;
-    data_div.id="movies";
-    data.forEach(function(el){
-        let div=document.createElement("div");
+let box=document.querySelector("#basic")
+function showData(items){
+    box.innerHTML=null;
+    items.map(function(el){
+      
+    let div=document.createElement("div")
+    let images=document.createElement("img")
+    images.setAttribute("src",el.images)
+    let titles=document.createElement("h3")
+    titles.innerText=el.titles
+    let sidetitles=document.createElement("p")
+    sidetitles.innerText=el.sidetitles
+    let price=document.createElement("h5")
+    price.innerText=`Price: $${el.price}`
+    let rating=document.createElement("h5")
+    rating.innerText=`Rating: ${el.rating}`
+  
+    let button=document.createElement("button")
+    button.innerText="Add To Bag"
+    button.id="btn"
+    button.style.background="black";
+    button.style.color="white";
+    
+  
+  div.append(images,titles,sidetitles,price,rating,button)
+  box.append(div)
+  
+  })
+
+}
+
+showData(items)
+
+// name functionality
+const inp=document.getElementById('type')
+
+  inp.addEventListener('change',()=>{
+      filteredResult(items)
+  })
+
+    const filteredResult =(items)=>{
+
+        const selected=document.getElementById('type').value 
+
+        const new_data= items.filter(function(el){
+
+            
+        return el.sidetitles==selected
+
+        })
+
+    console.log(new_data)
+   showData(new_data)
+  }
+// Products functionality
+
+
+
+const pro=document.getElementById("name")
+
+  pro.addEventListener("change",()=>{
+      filteredResults(items)
+  })
+
+  const filteredResults =(items)=>{
+
+    const selected=document.getElementById("name").value 
+
+   const new_data= items.filter(function(el){
+
         
-        let name=document.createElement("p");
-        name.innerHTML=`Name: ${el.name}`;
+        return el.titles==selected
 
-        let year=document.createElement("p");
-        year.innerHTML=`Year: ${el.year}`;
-
-        let rating=document.createElement("p");
-        rating.innerHTML=`Rating: ${el.rating}`;
-
-        let img=document.createElement("img");
-        img.id="poster";
-        img.src=el.img;
-
-        div.append(img,name,year,rating);
-        data_div.append(div);
     })
-}
+    console.log(new_data)
+   showData(new_data)
+  }
+
+//sorting functionality
+
+const output=document.getElementById('sort')
+
+ output.addEventListener('change',()=>{
+
+  const selected= document.getElementById('sort').value
+    
+     if(selected=='Price Low to High')
+     {
+         lowToHigh(items)
+     }else{
+        highToLow(items)
+     }
+ })
+ function lowToHigh(items){
+
+   items.sort(function(a,b){
+
+     return (a.price) - (b.price)
+   })
+
+    console.log(items)
+    showData(items)
+ }
 
 
-function sortLH(){
-    let data=JSON.parse(localStorage.getItem("movies"));
-    data=data.sort((a, b) => a.rating-b.rating);
-    appendMovies(data);
-}
+ function highToLow(items){
 
-function sortHL(){
-    let data=JSON.parse(localStorage.getItem("movies"));
-    data=data.sort((a, b) => b.rating - a.rating);
-    appendMovies(data);
-}
+   items.sort(function(a,b){
 
-function sortTopRating(){
-    let data=JSON.parse(localStorage.getItem("movies"));
-    data=data.sort((a, b) => b.rating - a.rating);
-    appendMovies(data);
-}
+      return (b.price)-(a.price)
+   })
+
+   console.log(items)
+   showData(items)
+ }
